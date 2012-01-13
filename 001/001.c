266898434b2fc660aba34802f649138a0f2eb26e
Stanley Tan <me@stnly.com>
2012-01-13 13:05:47 +0800

    Added C code to problem 1.

---
+++ 001/001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int sum = 0, num = 0;
+    for (num = 0; num < 1000; num++)
+    {
+        if (num%3 == 0 || num % 5 == 0) 
+        {
+            sum += num;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
